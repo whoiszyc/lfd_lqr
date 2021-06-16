b4f0437567656cc9efc969ef16a4cf9520377928
Yichen <31248391+whoiszyc@users.noreply.github.com>
2021-06-16 11:29:58 -0500

    compare two cases (small and large r)

--- case_SG_WTG.py
+++ case_SG_WTG.py
@@ -24,8 +24,8 @@ Bu = B[:, [0]]
 Bd = B[:, [1]]
 
 # simulation test
-delta_t = 0.01 # time step size (seconds)
-t_max = 3.0
+delta_t = 0.01  # time step size (seconds)
+t_max = 20.0
 T_series = np.linspace(0.0, t_max, int(t_max / delta_t))  # Get timesteps
 x0 = np.array([[0], [0], [0], [0]])  # Initial condition
 xf = np.array([[0], [0], [0], [0]])  # Final condition
@@ -40,8 +40,8 @@ Acl = A + Bu @ K_true  # closed-loop A
 Xt_cl = dyn_sim_discrete_time(Acl, Bu, Bd, x0, np.array([[0]]), np.array([[0.1]]), T_series)  # simulate closed-loop
 
 # NOC control
-U_noc = dyn_noc(A, Bu, Bd, x0, xf, [0.1], T_series)
-Xt_noc = dyn_sim_discrete_time(A, Bu, Bd, x0, U_noc, np.array([[0.1]]), T_series)
+U_noc = dyn_noc(A, Bu, Bd, x0, xf, [0.1], T_series)  # solve NOC problem
+Xt_noc = dyn_sim_discrete_time(A, Bu, Bd, x0, U_noc, np.array([[0.1]]), T_series)  # simulate NOC
 f = plt.figure(figsize=(12, 8))
 ax = f.add_subplot(111)
 ax.plot(T_series, U_noc[0, :], label="Control")
@@ -62,6 +62,9 @@ K_noc_Kalman = policy_fitting_with_a_kalman_constraint(L, r, Xt_noc.T[0:50, :], 
 Acl_noc_Kalman = A + Bu @ K_noc_Kalman  # closed-loop A
 Xt_cl_noc_Kalman = dyn_sim_discrete_time(Acl_noc_Kalman, Bu, Bd, x0, np.array([[0]]), np.array([[0.1]]), T_series)  # simulate closed-loop
 
+print("Learn NOC w/o Kalman constraint {}".format(K_noc))
+print("Learn NOC w Kalman constraint {}".format(K_noc_Kalman))
+
 # test learning with sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
@@ -81,27 +84,27 @@ ax.legend(loc='lower right', fontsize=12, ncol=3)
 f = plt.figure(figsize=(12, 8))
 ax = f.add_subplot(311)
 ax.plot(T_series, Xt[0, :], label="Open-loop")
-ax.plot(T_series, Xt_cl[0, :], label="LRQ")
-ax.plot(T_series, Xt_noc[0, :], label="NOC")
+# ax.plot(T_series, Xt_cl[0, :], label="LRQ")
+# ax.plot(T_series, Xt_noc[0, :], label="NOC")
 ax.plot(T_series, Xt_cl_noc[0, :], label="Leanred NOC")
 ax.plot(T_series, Xt_cl_noc_Kalman[0, :], linestyle='--', label="Leanred NOC w Kalman")
 ax.set_ylabel("Frequency (Hz)")
 ax.legend(loc='lower right', fontsize=12, ncol=3)
 bx = f.add_subplot(312)
 bx.plot(T_series, Xt[1, :], label="Open-loop")
-bx.plot(T_series, Xt_cl[1, :], label="LRQ")
-bx.plot(T_series, Xt_noc[1, :], label="NOC")
+# bx.plot(T_series, Xt_cl[1, :], label="LRQ")
+# bx.plot(T_series, Xt_noc[1, :], label="NOC")
 bx.plot(T_series, Xt_cl_noc[1, :], label="Leanred NOC")
 bx.plot(T_series, Xt_cl_noc_Kalman[1, :], linestyle='--', label="Leanred NOC w Kalman")
 bx.set_ylabel("Mechenical (PU)")
 bx.legend(loc='lower right', fontsize=12, ncol=3)
 cx = f.add_subplot(313)
 cx.plot(T_series, Xt[3, :], label="Open-loop")
-cx.plot(T_series, Xt_cl[3, :], label="LRQ")
-cx.plot(T_series, Xt_noc[3, :], label="NOC")
+# cx.plot(T_series, Xt_cl[3, :], label="LRQ")
+# cx.plot(T_series, Xt_noc[3, :], label="NOC")
 cx.plot(T_series, Xt_cl_noc[3, :], label="Leanred NOC")
 cx.plot(T_series, Xt_cl_noc_Kalman[3, :], linestyle='--', label="Leanred NOC w Kalman")
 cx.set_xlabel("Time (s)")
 cx.set_ylabel("WTG Speed (PU)")
 cx.legend(loc='lower left', fontsize=12, ncol=3)
-plt.savefig("fig_case_comp.pdf")
+plt.savefig("fig_case_comp_small_r_t20.pdf")
